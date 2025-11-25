df795e0a630e891d343e3f691b22d8bf98459650
GauravLamichhane <gaurav.lamichhane02@gmail.com>
2025-11-25 18:00:43 +0545

    "Render redeploys backend"

--- server/backend/settings.py
+++ server/backend/settings.py
@@ -175,6 +175,7 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",  # Vite default port
     "http://127.0.0.1:3000",
     "http://127.0.0.1:5173",
+    "https://task-management-platform-3s78quk1x-gauravlamichhanes-projects.vercel.app",
 ]
 
 CORS_ALLOW_CREDENTIALS = True
